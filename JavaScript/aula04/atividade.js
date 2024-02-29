cafe7fba25eb82d655c45d15da75bfaeba8c2f78
Pedro Henrique Oliveira Silva <pedro.o.silva975@gmail.com>
2024-02-29 20:07:36 -0300

    Dia 2 + atividade update

---
+++ JavaScript/aula04/atividade.js
@@ -0,0 +1,22 @@
+/* Atividade de constante e variável:
+
+Pedro Oliveira Silva tem 18 anos, pesa 68kg
+tem 1.74 de altura e seu IMC é de xxxxxx
+Pedro Oliveira Silva nasceu em xxxx
+ */
+
+const nome = "Pedro Oliveira";
+const sobrenome = "Silva";
+const idade = 18;
+const peso = 68;
+const alturaEmM = 1.74;
+let IMC = peso / (alturaEmM*alturaEmM);
+const anoAtual = 2024;
+const anoDeNascimento = (anoAtual-1)-idade; // Eu nasci em Abril(ainda não era abril quando fiz esse código), para não fazer um algoritmo com sistema de meses, eu subtraí 1 do ano atual para poder dar o resultado corredo do ano de nascimento.
+console.log(nome, sobrenome, 'tem', idade, 'anos, pesa', peso+'kg, tem', alturaEmM, 'de altura e seu IMC é de', IMC);
+console.log(nome, sobrenome, 'nasceu em', anoDeNascimento); 
+
+// Uma forma de evitar usar vírgula para separar ou espaçar variáveis ou constantes é utilizando a crase para toda a string do console e para sinlaizar o que é uma letORconst usa-se ${letORconst} EX: 
+console.log(`Olá meu nome é ${nome} e eu tenho ${idade} anos`) 
+
+
