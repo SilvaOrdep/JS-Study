c59035fa19296825d121ff3f72d32afe3aa45482
Pedro Henrique Oliveira Silva <pedro.o.silva975@gmail.com>
2024-03-04 19:39:06 -0300

    Atividade aula 08

---
+++ JavaScript/aula08/atividade.js
@@ -0,0 +1,19 @@
+/* Atividade de variáveis
+criar 3 variáveis, A B C
+e atribuir os valores de uma para outra fazendo 
+A = B
+B = C
+C = A
+*/
+
+let varA = "A"; // B
+let varB = "B"; // C
+let varC = "C"; // A
+let varD;
+
+varD = varA
+varA = varB
+varB = varC
+varC = varD
+
+console.log(varA, varB, varC);
