58a82cc6a68ad3410a302b1f49a35220c4ec443f
Pedro Henrique Oliveira Silva <pedro.o.silva975@gmail.com>
2024-03-12 18:33:58 -0300

    Folder update 2

--- JavaScript/aula01/aula01.js
+++
@@ -1,8 +0,0 @@
-console.log('Olá meu nome é Pedro Silva e comecei a estudar JavaScript no ano de', 2024+'.');
-
-// introdução ao uso do comando console.log(); 
-// string = linha de texto, e para setar texto utiliza-se 'text', "text" e `text`;
-// para colocar aspas simples no texto carece estar dentro de aspas duplas e virse versa, para colocar amabas, utiliza-se crase.
-// number sempre fora das aspas e etc.
-// para pontuar uma frase ou seja o que for, quando terminada em number, utiliza-se +'text or .' para evitar o espaço que a vírgula adiciona.
- 
--- JavaScript/aula02/aula02.js
+++
@@ -1,6 +0,0 @@
-console.log('teste de implementação de javascript no console de uma página html')
-console.log('Horário de Brasília: GMT-'+3+'.')
-
-/*  Linkar javascript ao html:
-colocar a tag <script src="local do arquivo .js"><script>
-*/
