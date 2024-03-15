97c9e295384cc5d750b73bb2f8f8b8b6a3ea6116
Pedro Henrique Oliveira Silva <pedro.o.silva975@gmail.com>
2024-03-15 13:36:55 -0300

    Aula 02

---
+++ Java Script Lógica de Programação/aula02/aula02.js
@@ -0,0 +1,10 @@
+/* Operadores lógicos
+&& - And, onde todas as expressões precisam ser verdadeiras
+|| Or, ou 
+! - Not
+EXEMPLOS: */
+const login = "OrdepSilva";
+const senha = 123456;
+
+const user = login === "OrdepSilva" && senha === 123456;
+console.log(user);
