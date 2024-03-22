1b9848d4aa58288fe61230f783afef8c4b04da9b
Pedro Henrique Oliveira Silva <pedro.o.silva975@gmail.com>
2024-03-22 19:25:18 -0300

    aula 07 - Swich/Case

--- atividades EXTRAS/conversor ASCII/index.js
+++ atividades EXTRAS/conversor ASCII/index.js
@@ -1,9 +1,12 @@
 // estudo de hash map para montar um conversor ascii:
 const ascii = new Map();
 
-ascii.set("Nathan", "555-0182");
+ascii.set(101, "e");
 ascii.set("Jane", "555-0182");
 
-console.log(ascii.get("Nathan")); // 555-0182
-console.log(ascii.size); // 2
+console.log(ascii.get(101)); 
+
 // ......
+
+const banco = ["a", "b", "c", "d"]
+ 
