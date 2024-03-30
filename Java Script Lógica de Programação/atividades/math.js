46087dcce4e10df01bb8d28b888d82b3f31bf983
Pedro Silva <161620501+SilvaOrdep@users.noreply.github.com>
2024-03-30 20:24:07 -0300

    object math

---
+++ Java Script Lógica de Programação/atividades/math.js
@@ -0,0 +1,9 @@
+const randomNum = Math.random() * 100
+//console.log(randomNum);
+
+const randomNum2 = Math.floor(Math.random() * 100)
+console.log(randomNum2);
+
+console.log(Math.ceil(43.07));
+
+console.log(Number.isInteger(2024));
