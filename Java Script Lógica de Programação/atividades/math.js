2c27fb1c5faf4c89936838bde457d3ab254b380b
Pedro Silva <161620501+SilvaOrdep@users.noreply.github.com>
2024-03-31 19:30:22 -0300

    Update math.js

--- Java Script Lógica de Programação/atividades/math.js
+++ Java Script Lógica de Programação/atividades/math.js
@@ -1,9 +1,37 @@
+/* 
+    Math -> Matématica 
+        - pow(2,2) / potência / 2² = 4
+        - sqrt(25,2) / raiz]
+        - ceil / arredondar para cima
+        - floor / arredondar para baixo
+        - random() / número aleatório entre 0 e 1
+*/
+
+// pow
+const pot = Math.pow(2,2);
+console.log(pot);
+
+// sqrt
+const raiz = Math.sqrt(25,2);
+console.log(raiz);
+
+// ceil
+console.log(Math.ceil(43.07));
+
+// floor
+console.log(Math.floor(43.07));
+
+// random
 const randomNum = Math.random() * 100
 //console.log(randomNum);
 
+
+// random com floor
 const randomNum2 = Math.floor(Math.random() * 100)
 console.log(randomNum2);
 
-console.log(Math.ceil(43.07));
+// random com minimo e maximo determinado
+const randomNum3 = Math.random() * (max - min) + min;
 
+// extra
 console.log(Number.isInteger(2024));
