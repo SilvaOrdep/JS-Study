6b78759ece0fc5b6754df402d8829fadd998e52f
Pedro Henrique Oliveira Silva <pedro.o.silva975@gmail.com>
2024-04-05 22:43:15 -0300

    ascii conversor - update ver.óvulo

--- atividades EXTRAS/conversor ASCII/modelo/assets/js/index.js
+++ atividades EXTRAS/conversor ASCII/modelo/assets/js/index.js
@@ -1,12 +1,31 @@
-// estudo de hash map para montar um conversor ascii:
-const ascii = new Map();
+const form = document.querySelector('.form');
+// Função para converter uma string em ASCII decimal
+function convAsciII(inputStr) { 
+  let asciiBank = []; 
+  for (let i = 0; i < inputStr.length; i++) { 
+    // Obtém o valor ASCII decimal de cada caractere 
+    let valorEmAscii = inputStr.charCodeAt(i); 
+    asciiBank.push(valorEmAscii); // Armazena o valor ASCII na lista
+  }
+  return asciiBank; // Retorna a lista com os valores ASCII
+}
 
-ascii.set(101, "e");
-ascii.set("Jane", "555-0182");
+function imprimirValores(inputStr) { 
+  inputStr.preventDefault();
+  let asciiBank = convAsciII(inputStr); 
+  for (let i = 0; i < asciiBank.length; i++) {
+    console.log(`O caractere "${input[i]}" tem o código ASCII decimal ${asciiBank[i]}`);
+  } 
+} 
 
-console.log(ascii.get(101)); 
+form.addEventListener('submit', imprimirValores);
 
-// ......
+// Exemplo de uso
+let input = ("canso"); 
+imprimirValores(input); 
 
-const banco = ["a", "b", "c", "d"]
- 
+// faltando ligar os input de texto do hmtl aqui
+// faltando ajustar o preventDefault de submit (n tá funfando)
+// faltando fazer o caminho de volta, ascii to string
+// ajustes no css e tamanho do input 
+// etc...
