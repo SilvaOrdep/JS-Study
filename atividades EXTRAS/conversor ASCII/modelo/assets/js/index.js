b8915fa67df79cba89de7032282a8caf5e8bdeb7
Pedro Henrique Oliveira Silva <pedro.o.silva975@gmail.com>
2024-04-06 21:53:53 -0300

    Update conversor Ascii

--- atividades EXTRAS/conversor ASCII/modelo/assets/js/index.js
+++ atividades EXTRAS/conversor ASCII/modelo/assets/js/index.js
@@ -1,31 +1,37 @@
-const form = document.querySelector('.form');
-// Função para converter uma string em ASCII decimal
-function convAsciII(inputStr) { 
-  let asciiBank = []; 
-  for (let i = 0; i < inputStr.length; i++) { 
-    // Obtém o valor ASCII decimal de cada caractere 
-    let valorEmAscii = inputStr.charCodeAt(i); 
-    asciiBank.push(valorEmAscii); // Armazena o valor ASCII na lista
+function escopoPrincipal() {
+
+  const form = document.querySelector('.form');
+  const strInput = document.querySelector('.txt');
+  const asciiHtmlInput = document.querySelector('.asciiInput');
+  const resultado = document.querySelector('.resultado')
+
+  function receberEvento(evento) {
+    evento.preventDefault();
+    // Função para converter uma string em ASCII decimal
+    function convAsciII(inputStr) {
+      let asciiBank = [];
+      for (let i = 0; i < inputStr.length; i++) {
+        // Obtém o valor ASCII decimal de cada caractere 
+        let valorEmAscii = inputStr.charCodeAt(i);
+        asciiBank.push(valorEmAscii); // Armazena o valor ASCII na lista
+      }
+      return asciiBank; // Retorna a lista com os valores ASCII
+    }
+
+    function imprimirValores(inputStr) {
+      let asciiBank = convAsciII(inputStr);
+      let result = '';
+      for (let i = 0; i < asciiBank.length; i++) {
+        result += `${asciiBank[i]}`
+      }
+      resultado.innerHTML = `String para ASCII: ${result}`
+
+    }
+    imprimirValores(strInput.value);
   }
-  return asciiBank; // Retorna a lista com os valores ASCII
+
+  form.addEventListener('submit', receberEvento);
 }
+escopoPrincipal();
 
-function imprimirValores(inputStr) { 
-  inputStr.preventDefault();
-  let asciiBank = convAsciII(inputStr); 
-  for (let i = 0; i < asciiBank.length; i++) {
-    console.log(`O caractere "${input[i]}" tem o código ASCII decimal ${asciiBank[i]}`);
-  } 
-} 
-
-form.addEventListener('submit', imprimirValores);
-
-// Exemplo de uso
-let input = ("canso"); 
-imprimirValores(input); 
-
-// faltando ligar os input de texto do hmtl aqui
-// faltando ajustar o preventDefault de submit (n tá funfando)
-// faltando fazer o caminho de volta, ascii to string
-// ajustes no css e tamanho do input 
-// etc...
+// Missão principal = fazer o caminho de volta, minha ideia é fazer uma condição onde se o usurario digiar o result de imprimirValores() no input de ASCII ele vai retornar o valor string do input de string, não vai fazer necessáriamente uma conversão e sim uam gambiarra, MAS é o caminho de volta 
