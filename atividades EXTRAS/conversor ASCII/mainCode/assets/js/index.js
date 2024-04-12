6e1eff702a779ec10ba590ed592a235d53808b81
Pedro Henrique Oliveira Silva <pedro.o.silva975@gmail.com>
2024-04-12 14:19:38 -0300

    Ascii Conversor update

--- atividades EXTRAS/conversor ASCII/mainCode/assets/js/index.js
+++ atividades EXTRAS/conversor ASCII/mainCode/assets/js/index.js
@@ -3,35 +3,41 @@ function escopoPrincipal() {
   const form = document.querySelector('.form');
   const strInput = document.querySelector('.txt');
   const asciiHtmlInput = document.querySelector('.asciiInput');
-  const resultado = document.querySelector('.resultado')
 
   function receberEvento(evento) {
     evento.preventDefault();
-    // Função para converter uma string em ASCII decimal
     function convAsciII(inputStr) {
       let asciiBank = [];
       for (let i = 0; i < inputStr.length; i++) {
-        // Obtém o valor ASCII decimal de cada caractere 
         let valorEmAscii = inputStr.charCodeAt(i);
-        asciiBank.push(valorEmAscii); // Armazena o valor ASCII na lista
+        asciiBank.push(valorEmAscii);
       }
-      return asciiBank; // Retorna a lista com os valores ASCII
+      return asciiBank;
     }
 
     function imprimirValores(inputStr) {
       let asciiBank = convAsciII(inputStr);
       let result = '';
       for (let i = 0; i < asciiBank.length; i++) {
-        result += `${asciiBank[i]}`
+        result += `${asciiBank[i]} `
       }
-      resultado.innerHTML = `String para ASCII: ${result}`
+      asciiHtmlInput.innerHTML = `${result}`
 
     }
     imprimirValores(strInput.value);
+
+    function convStr(inputAscii) {
+      let asciiArray = inputAscii.split(' ').map(num => parseInt(num.trim()));
+      let result = '';
+      for (let i = 0; i < asciiArray.length; i++) {
+        let valorEmStr = String.fromCharCode(asciiArray[i]);
+        result += valorEmStr;
+      }
+      strInput.innerHTML = result;
+    }
+    convStr(asciiHtmlInput.value)
   }
 
   form.addEventListener('submit', receberEvento);
 }
 escopoPrincipal();
-
-// Missão principal = fazer o caminho de volta, minha ideia é fazer uma condição onde se o usurario digiar o result de imprimirValores() no input de ASCII ele vai retornar o valor string do input de string, não vai fazer necessáriamente uma conversão e sim uam gambiarra, MAS é o caminho de volta, OBS: o input de ascii tá reservado e sem uso na const "asciiHtmlInput" acima.
