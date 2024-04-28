2c11a6be79fbf0d16ff232a702eced80774d2198
PedroSilva <pedro.o.silva975@gmail.com>
2024-04-28 19:23:28 -0300

    ascii update

--- atividades EXTRAS/conversor ASCII/mainCode/assets/js/index.js
+++ atividades EXTRAS/conversor ASCII/mainCode/assets/js/index.js
@@ -1,43 +1,40 @@
-function escopoPrincipal() {
+const form = document.querySelector(".form");
+const strInput = document.querySelector(".txt");
+const asciiHtmlInput = document.querySelector(".asciiInput");
 
-  const form = document.querySelector('.form');
-  const strInput = document.querySelector('.txt');
-  const asciiHtmlInput = document.querySelector('.asciiInput');
-
-  function receberEvento(evento) {
-    evento.preventDefault();
-    function convAsciII(inputStr) {
-      let asciiBank = [];
-      for (let i = 0; i < inputStr.length; i++) {
-        let valorEmAscii = inputStr.charCodeAt(i);
-        asciiBank.push(valorEmAscii);
-      }
-      return asciiBank;
+function receberEvento(evento) {
+  evento.preventDefault();
+  function convAsciII(inputStr) {
+    let asciiBank = [];
+    for (let i = 0; i < inputStr.length; i++) {
+      let valorEmAscii = inputStr.charCodeAt(i);
+      asciiBank.push(valorEmAscii);
     }
-
-    function imprimirValores(inputStr) {
-      let asciiBank = convAsciII(inputStr);
-      let result = '';
-      for (let i = 0; i < asciiBank.length; i++) {
-        result += `${asciiBank[i]} `
-      }
-      asciiHtmlInput.innerHTML = `${result}`
-
-    }
-    imprimirValores(strInput.value);
-
-    function convStr(inputAscii) {
-      let asciiArray = inputAscii.split(' ').map(num => parseInt(num.trim()));
-      let result = '';
-      for (let i = 0; i < asciiArray.length; i++) {
-        let valorEmStr = String.fromCharCode(asciiArray[i]);
-        result += valorEmStr;
-      }
-      strInput.innerHTML = result;
-    }
-    convStr(asciiHtmlInput.value)
+    return asciiBank;
   }
 
-  form.addEventListener('submit', receberEvento);
+  function imprimirValores(inputStr) {
+    let asciiBank = convAsciII(inputStr);
+    let result = "";
+    for (let i = 0; i < asciiBank.length; i++) {
+      result += `${asciiBank[i]} `;
+    }
+    asciiHtmlInput.innerHTML = `${result}`;
+  }
+  imprimirValores(strInput.value);
+
+  function convStr(inputAscii) {
+    let asciiArray = inputAscii
+      .split(" ")
+      .map((num) => parseInt(num.trim()));
+    let result = "";
+    for (let i = 0; i < asciiArray.length; i++) {
+      let valorEmStr = String.fromCharCode(asciiArray[i]);
+      result += valorEmStr;
+    }
+    strInput.innerHTML = result;
+  }
+  convStr(asciiHtmlInput.value);
 }
-escopoPrincipal();
+
+form.addEventListener("submit", receberEvento);
